90623fde87ff29bf6c997efad752de149f478eaa
kalyazin.nik <kalyazin.nik@yandex.ru>
2025-04-26 11:43:58 +0300

    очистка кода от комментариев

--- curb.ui/src/router.js
+++ curb.ui/src/router.js
@@ -1,24 +1,10 @@
-//import { createRouter, createWebHistory } from 'vue-router';
-//import WeatherForecast from './components/WeatherForecast.vue';
-
-//const routes = [
-//  { path: '/weather', component: WeatherForecast }
-//];
-
-//const router = createRouter({
-//  history: createWebHistory(),
-//  routes,
-//});
-
-//export default router;
-
 import { createRouter, createWebHistory } from 'vue-router';
 import TheWelcome from './components/TheWelcome.vue';
 import WeatherForecast from './components/WeatherForecast.vue';
 
 const routes = [
-  { path: '/', component: TheWelcome }, // Главная страница
-  { path: '/weather', component: WeatherForecast }, // Страница с прогнозом погоды
+  { path: '/', component: TheWelcome },
+  { path: '/weather', component: WeatherForecast },
 ];
 
 const router = createRouter({
